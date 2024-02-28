fe5376c47e39968a0e381361393858b1b1ff6631
Jean Santos <shadowjean911@gmail.com>
2024-02-28 08:13:38 -0300

    feat: add light and dark themes

---
+++ src/styles/Themes.jsx
@@ -0,0 +1,20 @@
+export const lightTheme = {
+    body: '#fff',
+    text: '#000',
+    link: '#000',
+    span: '#ff0000',
+    button: '#ff0000',
+    textContainer: '#ff0000',
+    hover: '#b10202', 
+  };
+  
+  export const darkTheme = {
+    body: '#252537',
+    text: '#fff',
+    link: '#fff',
+    span: '#ffbb33',
+    button: '#ffbb33',
+    textContainer: '#ffbb33',
+    hover: '#af7502',
+  };
+  
