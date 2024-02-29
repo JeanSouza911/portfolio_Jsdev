02078fe8498085d3eec1a9646b86765e2c45ea1f
Jean Santos <shadowjean911@gmail.com>
2024-02-29 08:19:09 -0300

    style: add styled-components for About component

---
+++ src/styles/AboutStyles.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+   margin: 5rem auto;
+   padding: 1rem 1rem;
+   width: 70%;
+   overflow-wrap: break-word;
+   box-shadow: 0 0 1rem;
+   border-radius: 10px;
+`;
+
+export const Heading = styled.h1`
+    font-size: 3rem;
+    margin: 1rem;
+`;
+
+export const Paragraph = styled.p`
+    font-size: 1.7rem;
+    margin: 1rem;
+`;
+
+export const Footer = styled.footer`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 2rem;
+    gap: 1rem;
+`
