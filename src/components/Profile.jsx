96c3eb087e89998a4b0a4f907dd93c83a2bb41f5
Jean Santos <shadowjean911@gmail.com>
2024-02-29 08:22:12 -0300

    feat: add Profile component with user profile image

---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import {ProfileImageContainer, ProfileImage} from '../styles/ProfileStyles';
+
+const Profile = () => {
+    return (
+      <ProfileImageContainer>
+        <ProfileImage src='/img/jean.png' alt="Nome da pessoa" />
+      </ProfileImageContainer>
+    );
+  };
+  
+  export default Profile;
