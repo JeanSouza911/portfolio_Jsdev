603d2c5ee5af1bf10bdd5f24750541a00a2f64ba
Jean Santos <shadowjean911@gmail.com>
2024-02-29 21:01:19 -0300

    style: add styled-components for Profile component

---
+++ src/styles/ProfileStyles.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const ProfileImageContainer = styled.div`
+
+`;
+
+export const ProfileImage = styled.img`
+  width: 450px; 
+  height: 450px; 
+  border-radius: 50%;
+  margin: 3rem 2rem;
+
+`;
