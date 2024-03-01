eb244b71b3a43d8eb4109db89282613c08c0084b
Jean Santos <shadowjean911@gmail.com>
2024-02-29 21:07:57 -0300

    feat: add Contact component with contact information and links

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,30 @@
+import { Container, ContactInfo, ContactItem, Link } from '../styles/ContactStyles';
+import { SiGmail } from 'react-icons/si';
+import { FaLinkedin, FaWhatsapp } from 'react-icons/fa';
+
+const Contact = () => {
+  return (
+    <Container>
+      <h1>Entre em contato comigo para discutir oportunidades de colaboração ou apenas para dizer oi!</h1>
+      <ContactInfo>
+        <ContactItem> 
+          <Link href='mailto:shadowjean911@gmail.com'><SiGmail style={{ color: `#d44638`}}/></Link>
+          <p>Me mande um e-mail!</p>
+        </ContactItem>
+        
+        <ContactItem> 
+          <Link href="https://www.linkedin.com/in/jean-souza-79b491160/"><FaLinkedin style={{ color: `#0077b5`}} /></Link>
+          <p>Visite meu perfil no LinkedIn!</p>
+        </ContactItem>
+        
+        <ContactItem> 
+          <Link href="https://wa.me/5516997062310"><FaWhatsapp style={{ color: `#25d366`}} /></Link>
+          <p>Me chame no Whatsapp!</p>
+        </ContactItem>
+        <></>
+      </ContactInfo>
+    </Container>
+  );
+}
+
+export default Contact;
