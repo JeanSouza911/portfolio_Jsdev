c06b12955a8c57630b8d1197bcf488b978c0270f
Jean Santos <shadowjean911@gmail.com>
2024-04-02 11:53:24 -0300

    feat: integrate AppWithTransition component with React Router in index.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AppWithTransition from './components/AppWithFadeTransition.jsx'; // Importe o AppWithTransition
 
-import App from './App.jsx';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Projects from './pages/Projects.jsx';
@@ -14,13 +14,27 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route element={<App />}>
-          <Route path='/' element={<Home />}/>
-          <Route path='about' element={<About />}/>
-          <Route path='projects' element={<Projects />}/>
-          <Route path='contact' element={<Contact />}/>
+        <Route
+          element={<AppWithTransition />}
+        >
+          <Route
+            index
+            element={<Home />}
+          />
+          <Route
+            path='about'
+            element={<About />}
+          />
+          <Route
+            path='projects'
+            element={<Projects />}
+          />
+          <Route
+            path='contact'
+            element={<Contact />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
-)
+);
