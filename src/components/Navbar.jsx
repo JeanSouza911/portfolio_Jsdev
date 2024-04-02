9cd86b41fe34bdeee6fdb51885e1543107f02fae
Jean Santos <shadowjean911@gmail.com>
2024-04-02 11:44:48 -0300

    chore: update Navbar component to use state for menu and small screen detection

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,72 @@
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Nav, Menu, MenuItem, StyledLink } from "../styles/NavStyles";
-import { FaSun, FaMoon } from 'react-icons/fa';
+import { FaSun, FaMoon, FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = ({ toggleTheme, theme }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 595px)');
+    setIsSmallScreen(mediaQuery.matches);
+
+    const handleResize = (e) => {
+      setIsSmallScreen(e.matches);
+    };
+
+    mediaQuery.addEventListener('change', handleResize);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleResize);
+    };
+  }, []);
+
+  const handleToggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleCloseMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <Nav>
-      <Menu>
-        <MenuItem>
-          <StyledLink to='/'>Home</StyledLink>
-          <StyledLink to='about'>Sobre</StyledLink>
-          <StyledLink to='projects'>Projetos</StyledLink>
-          <StyledLink to='contact'>Contato</StyledLink>
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <div>
           {theme === 'light' ? (
-            <FaSun onClick={toggleTheme} style={{ cursor: 'pointer'}} />
+            <FaMoon onClick={toggleTheme} style={{ cursor: 'pointer' }} />
           ) : (
-            <FaMoon onClick={toggleTheme} style={{ cursor: 'pointer'}} />
+            <FaSun onClick={toggleTheme} style={{ cursor: 'pointer' }} />
           )}
-        </MenuItem>
-      </Menu>
+        </div>
+        {isSmallScreen && (
+          <div onClick={handleToggleMenu} style={{ cursor: 'pointer' }}>
+            {isMenuOpen ? <FaTimes /> : <FaBars />}
+          </div>
+        )}
+      </div>
+      {isSmallScreen && isMenuOpen && (
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: '#333', padding: '10px', position: 'absolute', width:'100%', zIndex: '1', height: '10rem' }}>
+          <StyledLink to='/' onClick={handleCloseMenu}>Home</StyledLink>
+          <StyledLink to='/about' onClick={handleCloseMenu}>Sobre</StyledLink>
+          <StyledLink to='/projects' onClick={handleCloseMenu}>Projetos</StyledLink>
+          <StyledLink to='/contact' onClick={handleCloseMenu}>Contato</StyledLink>
+        </div>
+      )}
+      {!isSmallScreen && (
+        <Menu style={{ display: isMenuOpen ? 'block' : 'flex' }}>
+          <MenuItem>
+            <StyledLink to='/' onClick={handleCloseMenu}>Home</StyledLink>
+            <StyledLink to='/about' onClick={handleCloseMenu}>Sobre</StyledLink>
+            <StyledLink to='/projects' onClick={handleCloseMenu}>Projetos</StyledLink>
+            <StyledLink to='/contact' onClick={handleCloseMenu}>Contato</StyledLink>
+          </MenuItem>
+        </Menu>
+      )}
     </Nav>
   );
-}
+};
 
 Navbar.propTypes = {
   toggleTheme: PropTypes.func.isRequired,
