d34596386548f4523c2b5059fc550bd629e77c9f
Jean Santos <shadowjean911@gmail.com>
2024-04-02 11:52:09 -0300

    feat: add AppWithTransition component for using FadeTransition with React Router

---
+++ src/components/AppWithFadeTransition.jsx
@@ -0,0 +1,15 @@
+import { useLocation } from 'react-router-dom';
+import FadeTransition from '../components/FadeTransition.jsx';
+import App from '../App.jsx';
+
+function AppWithTransition() {
+  const location = useLocation();
+
+  return (
+    <FadeTransition key={location.pathname}>
+      <App />
+    </FadeTransition>
+  );
+}
+
+export default AppWithTransition;
